c90110fb7cb101ff9367c7d03a03fe3122c6311d
Blieper <yoranmandema@eml.cc>
2018-01-05 22:10:13 +0100

    bla

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,65 +1,62 @@
-let questions = [];
+let currentQuestion = {rightAnswer: -1}
 let answerButtons = [];
-
-questions[0] = {
-    question: 'Is this a question?',
-    rightAnswer: 0,
-    answers: [
-        'yes',
-        'no',
-    ]
-}
-
-questions[1] = {
-    question: 'What is a quiz',
-    rightAnswer: 1,
-    answers: [
-        'something lol',
-        'idk',
-    ]
-}
-
-questions[2] = {
-    question: 'What is the meaning of life',
-    rightAnswer: 1,
-    answers: [
-        'to eat prawns',
-        'idk',
-        'to make js quizes',
-    ]
-}
+var score = 0;
 
 function setQuestion () {
-    // Getting a random index
-    let index = Math.round(Math.random() * (questions.length - 1));
+    if (questions.length) {
+        // Getting a random index
+        let index = Math.round(Math.random() * (questions.length - 1));
+        
+        let question = questions[index];
+        currentQuestion = question;
+    
+        // Set the question
+        document.getElementById("question").innerHTML = question.question;
+    
+        // Remove old buttons
+        for (i = 0; i < answerButtons.length; i++) {
+            let parent = document.getElementsByTagName("BODY")[0]; 
+            parent.removeChild(answerButtons[i]);
+        }
 
-    let question = questions[index];
+        answerButtons = [];
 
-    // Set the question
-    document.getElementById("question").innerHTML = question.question;
+        // Create new buttons
+        for (i = 0; i < question.answers.length; i++) {
+            let btn = document.createElement("BUTTON");       
+            let t = document.createTextNode(question.answers[i]);       
+            btn.appendChild(t);                                
+            document.body.appendChild(btn);
 
-    // Remove old buttons
-    for (i = 0; i < answerButtons.length; i++) {
-        console.log(i);
-        let parent = document.getElementsByTagName("BODY")[0]; 
-        parent.removeChild(answerButtons[i]);
+            answerButtons[i] = btn;
+
+            if (i == question.rightAnswer) {
+                let old = setQuestion;   
+
+                btn.onclick = function () {
+                    if (questions.length) {
+                        score++;
+                        document.getElementById("score").innerHTML = 'score: ' + score;
+                    }
+
+                    setQuestion();
+                };  
+            }else{
+                btn.onclick = setQuestion;            
+            }
+
+            btn.className = 'answer';
+        }
+
+        // Get rid of question so we don't repeat it
+        questions.splice(index,1);
+    }else{
+        // Remove old buttons
+        for (i = 0; i < answerButtons.length; i++) {
+            let parent = document.getElementsByTagName("BODY")[0]; 
+            parent.removeChild(answerButtons[i]);
+        }
+
+        document.getElementById("question").innerHTML = 'End of the quiz!';
     }
-
-    answerButtons = [];
-
-    // Create new buttons
-    for (i = 0; i < question.answers.length; i++) {
-        let btn = document.createElement("BUTTON");       
-        let t = document.createTextNode(question.answers[i]);       
-        btn.appendChild(t);                                
-        document.body.appendChild(btn);
-
-        answerButtons[i] = btn;
-
-        btn.onclick = setQuestion;
-        btn.className = 'answer';
-    }
-
-    // Get rid of question so we don't repeat it
-    questions.splice(index,1);
 }
---
+++ JS/questions.js
@@ -0,0 +1,95 @@
+let questions = [];
+
+questions[0] = {
+    question: 'Is this a question?',
+    rightAnswer: 0,
+    answers: [
+        'yes',
+        'no',
+    ]
+}
+
+questions[1] = {
+    question: 'What is a quiz',
+    rightAnswer: 1,
+    answers: [
+        'something lol',
+        'idk',
+    ]
+}
+
+questions[2] = {
+    question: 'What is the meaning of life',
+    rightAnswer: 1,
+    answers: [
+        'to eat prawns',
+        'idk',
+        'to make js quizes',
+    ]
+}
+
+questions[3] = {
+    question: 'Is this a question?',
+    rightAnswer: 0,
+    answers: [
+        'yes',
+        'no',
+    ]
+}
+
+questions[4] = {
+    question: 'What is a quiz',
+    rightAnswer: 1,
+    answers: [
+        'something lol',
+        'idk',
+    ]
+}
+
+questions[5] = {
+    question: 'What is the meaning of life',
+    rightAnswer: 1,
+    answers: [
+        'to eat prawns',
+        'idk',
+        'to make js quizes',
+    ]
+}
+
+questions[6] = {
+    question: 'What is the meaning of life',
+    rightAnswer: 1,
+    answers: [
+        'to eat prawns',
+        'idk',
+        'to make js quizes',
+    ]
+}
+
+questions[7] = {
+    question: 'Is this a question?',
+    rightAnswer: 0,
+    answers: [
+        'yes',
+        'no',
+    ]
+}
+
+questions[8] = {
+    question: 'What is a quiz',
+    rightAnswer: 1,
+    answers: [
+        'something lol',
+        'idk',
+    ]
+}
+
+questions[9] = {
+    question: 'What is the meaning of life',
+    rightAnswer: 1,
+    answers: [
+        'to eat prawns',
+        'idk',
+        'to make js quizes',
+    ]
+}
