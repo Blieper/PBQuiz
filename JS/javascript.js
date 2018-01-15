a30afe9a84c1909eadc940ec23633c9d4dc00043
Blieper <yoranmandema@eml.cc>
2018-01-15 19:25:20 +0100

    quick fix

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,24 +1,31 @@
 let currentQuestion = {rightAnswer: -1}
 let answerButtons = [];
-var score = 0;
+let score = 0;
+
+let scoreObject;
+let containerObject;
+let questionObject;
 
 window.addEventListener('load',init);
 
 function init () {
-    setQuestion();
+    scoreObject     = document.getElementById("score");
+    questionObject  = document.getElementById("question");
+    containerObject = document.getElementById("container");
+
+    setToStartScreen();
 }
 
 function timeOut(q, c = false) {
     if (c) {
-        document.getElementById("question").innerHTML = 'Correct!';
+        questionObject.innerHTML = 'Correct!';
     }else{
-        document.getElementById("question").innerHTML = 'Wrong! The right answer was ' + q.answers[q.rightAnswer] + '.';
+        questionObject.innerHTML = 'Wrong! The right answer was ' + q.answers[q.rightAnswer] + '.';
     }
         
     // Remove old buttons
-    for (i = 0; i < answerButtons.length; i++) {
-        let parent = document.getElementsByTagName("BODY")[0]; 
-        parent.removeChild(answerButtons[i]);
+    for (i = 0; i < answerButtons.length; i++) { 
+        containerObject.removeChild(answerButtons[i]);
     }
         
     answerButtons = [];
@@ -26,23 +33,38 @@ function timeOut(q, c = false) {
     setTimeout(setQuestion, 1500);         
 }
 
+function setToStartScreen () {
+    questionObject.innerHTML    = 'Welcome to this quiz. Click \'start\' to begin';
+    scoreObject.innerHTML       = 'START';
+    scoreObject.style.cursor    = 'pointer';
+    // VISIBILITY scoreObject.style.visibility = 'visible';
+
+    scoreObject.onclick = function () {
+        this.innerHTML      = 'Score: ' + score;
+        this.onclick        = null;
+        this.style.cursor   = 'default';
+        // VISIBILITY this.style.visibility = 'hidden';
+
+        setQuestion();
+    }
+}
+
 function setQuestion () {
     if (questions.length) {
         // Getting a random index
-        let index = Math.round(Math.random() * (questions.length - 1));
-        
-        let question = questions[index];
+        let index       = Math.round(Math.random() * (questions.length - 1));
+        let question    = questions[index];
         currentQuestion = question;
     
         // Set the question
-        document.getElementById("question").innerHTML = question.question;
+        questionObject.innerHTML = question.question;
 
         // Create new buttons
         for (i = 0; i < question.answers.length; i++) {
             let btn = document.createElement("BUTTON");       
-            let t = document.createTextNode(question.answers[i]);       
+            let t   = document.createTextNode(question.answers[i]);       
             btn.appendChild(t);                                
-            document.body.appendChild(btn);
+            containerObject.appendChild(btn);
 
             answerButtons[i] = btn;
 
@@ -50,7 +72,7 @@ function setQuestion () {
                 btn.onclick = function () {
                     if (questions.length) {
                         score++;
-                        document.getElementById("score").innerHTML = 'Score: ' + score;
+                        scoreObject.innerHTML = 'Score: ' + score;
                     }
                     
                     timeOut(question, true);
@@ -69,13 +91,18 @@ function setQuestion () {
     }else{
         // Remove old buttons
         for (i = 0; i < answerButtons.length; i++) {
-            let parent = document.getElementsByTagName("BODY")[0]; 
-            parent.removeChild(answerButtons[i]);
+            containerObject.removeChild(answerButtons[i]);
         }
 
-        document.getElementById("question").innerHTML = 'End of the quiz!';
+        questionObject.innerHTML = 'End of the quiz!';
 
-        document.getElementById("score").innerHTML = 'You scored ' + score + ' points! Reload the page to try again.';
+        // VISIBILITY scoreObject.style.visibility = 'visible';
+        
+        scoreObject.innerHTML = 'You scored ' + score + ' points! Click here to try again.';
+        scoreObject.style.cursor = 'pointer';
+        scoreObject.onclick = function () {
+            location.reload();
+        }
         
     }
 }
